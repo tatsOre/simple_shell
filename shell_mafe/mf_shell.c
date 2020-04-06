69d91ac9a5433a22a6bff06dd58fa4f1f0d21f76
mfcrespo <mafe_crespo@hotmail.com>
2020-04-06 12:39:06 +0000

    Add mf_shell.c

---
+++ shell_mafe/mf_shell.c
@@ -0,0 +1,62 @@
+#include "holberton.h"
+
+/**
+  * sig_out - checks if the signal was SIGINT, then prints a \n and the prompt
+  * @sign: the number of the signal
+  * Return: nothing, void
+  */
+void sig_out(int sign)
+{
+	if (sign == SIGINT)
+		write(STDOUT_FILENO, "\n$ ", 3);
+}
+
+/**
+  * main - Creates a loop for to receive call functions
+  * @argc: argument count
+  * @argv: an array of pointers to chars, arguments
+  * Return: 0, success, otherwise errors
+  */
+
+int main(__attribute__((unused)) int argc, char **argv)
+{
+	int loops, count;
+	char *buffer;
+	char **args;
+	size_t len;
+
+	loops = 1;
+
+	signal(SIGINT, sig_out);
+	while (1)
+	{
+		buffer = NULL;
+		len = 0;
+		args = NULL;
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "mafe$ ", 6);
+		if (getline(&buffer, &len, stdin) == EOF)
+		{
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			exit(EXIT_FAILURE);
+		}
+		count = countargs(buffer);
+		args = parser(buffer, count);
+		if (strcmp(args[0], "exit") == 0 &&
+				(strlen(args[0]) == strlen("exit")))
+		{
+			if (exitfunc(args, buffer) == -1)
+				errmess(argv, args, loops);
+		}
+		else if (args != NULL && args[0] != NULL)
+		{
+			if (interpreter(args) == -1)
+				errmess(argv, args, loops);
+		}
+		free(buffer);
+		free(args);
+	}
+	loops++;
+	return (0);
+}
