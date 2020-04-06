d861b65e1e7654083437036098333aac74ad4d7d
mfcrespo <mafe_crespo@hotmail.com>
2020-04-06 19:44:40 +0000

    Fix authors

--- shell_mafe/authors.sh
+++ shell_mafe/authors.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-set -e
-
-cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
-
-# see also ".mailmap" for how email addresses and names are deduplicated
-
 {
     cat <<-'EOH'
 # This file lists all individuals having contributed content to the repository.
