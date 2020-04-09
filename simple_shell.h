1def964d323fe940007c1c27a00a38ec63ec0f83
tatiana orejuela zapata <tatianaorezap@gmail.com>
2020-04-09 14:51:19 +0000

    Test

--- simple_shell.h
+++ simple_shell.h
@@ -1,8 +1,12 @@
 #ifndef SIMPLE_SHELL_H
 #define SIMPLE_SHELL_H
 
-#define _GNU_SOURCE
-#define DELIMITER " \n"
+#define BUFFSIZE 1024
+#define DELIMITER " :\n\t\r"
+#define  _GNU_SOURCE
+
+#include <stdarg.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,19 +14,40 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-#include <signal.h>
+#include <fcntl.h>
 
-char **_parser(char *line);
-int _executer(char **args, char **env);
+/*global environment variable */
+extern char **environ;
+
+int init_fileprogram(char **args);
+int init_builtin(char **args, char *buffer);
+int searchdir(char **command, char **args);
+int execute(char **args);
 
 
-/* String Handling Functions */
+/* String Handlers */
 int _strlen(char *str);
-char *_strdup(char *str);
 int _strncmp(char *s1, char *s2, int n);
 int _strcmp(char *s1, char *s2);
+char *_strdup(char *str);
 
-/* Handler Functions */
-char *path_concat(char *path, char *cmd);
+/* Path & Search Handlers */
+char *path_append(char *path, char *command);
+char *_getenv(char *name);
+int getpathfile(char **args);
+char **getpathdirectory(char *path);
+
+/* Built-ins Functions */
+void shb_env(void);
+void shb_exit(char **args, char *buffer);
+
+/* Simple Shell Handlers */
+void sig_out(int sign);
+void sighandle(int sign);
+void free_function(int n, ...);
+void errmess(char **argv, char **args, int loops);
+int countarg(char *buffer);
+char **get_tokens(char *buffer);
+void printfnum(int num);
 
 #endif /* SIMPLE_SHELL_H */
